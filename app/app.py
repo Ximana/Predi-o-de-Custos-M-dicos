637ebf81d4faa76e85288339d9b94a17a05ceb26
Ximana <pauloximana@gmail.com>
2024-06-13 05:21:54 +0100

    Modelo funcionando

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
 
 # Carregar o modelo
-filename = 'modelo_Predicao_de_Custos_Medicos.pkl'
-with open(filename, 'rb') as file:
+with open('modelo_Predicao_de_Custos_Medicos.pkl', 'rb') as file:
     model = pickle.load(file)
+
+# Carregar as colunas usadas durante o treinamento
+with open('colunas_treinamento.pkl', 'rb') as file:
+    treino_columns = pickle.load(file)
 
 @app.route('/')
 def home():
@@ -15,32 +19,33 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Pegar os dados do formulário
-        data = request.form
+    # Pegar os dados do formulário
+    data = request.form
+    novos_dados = pd.DataFrame({
+        'ID_Pedido': [data['ID_Pedido']],
+        'Data_Pedido': [data['Data_Pedido']],
+        'ID_Cliente': [data['ID_Cliente']],
+        'Segmento': [data['Segmento']],
+        'Pais': [data['Pais']],
+        'Cidade': [data['Cidade']],
+        'Estado': [data['Estado']],
+        'ID_Produto': [data['ID_Produto']],
+        'Categoria': [data['Categoria']],
+        'SubCategoria': [data['SubCategoria']]
+    })
 
-        # Extrair os dados do formulário e convertê-los para o tipo apropriado
-        id_pedido = float(data['ID_Pedido'])
-        data_pedido = float(data['Data_Pedido'])  # Supondo que você converta datas para um formato numérico apropriado
-        id_cliente = float(data['ID_Cliente'])
-        segmento = float(data['Segmento'])  # Transforme este campo adequadamente
-        pais = float(data['Pais'])          # Transforme este campo adequadamente
-        cidade = float(data['Cidade'])      # Transforme este campo adequadamente
-        estado = float(data['Estado'])      # Transforme este campo adequadamente
-        id_produto = float(data['ID_Produto'])
-        categoria = float(data['Categoria'])  # Transforme este campo adequadamente
-        subcategoria = float(data['SubCategoria'])  # Transforme este campo adequadamente
+    # Transformar variáveis categóricas em numéricas
+    novos_dados = pd.get_dummies(novos_dados, drop_first=True)
 
-        # Prepare os dados para previsão
-        input_data = np.array([[id_pedido, data_pedido, id_cliente, segmento, pais, cidade, estado, id_produto, categoria, subcategoria]])
+    # Garantir que a ordem das colunas é a mesma que foi usada no treinamento
+    novos_dados = novos_dados.reindex(columns=treino_columns, fill_value=0)
 
-        # Fazer a previsão
-        prediction = model.predict(input_data)[0]
+    # Fazer a previsão com o modelo treinado
+    y_pred_novos_dados = model.predict(novos_dados)
 
-        # Retornar o resultado
-        return render_template('index.html', prediction_text=f'Custo médico previsto: R${prediction:.2f}')
-    except ValueError as e:
-        return render_template('index.html', prediction_text=f'Erro na entrada dos dados: {str(e)}')
+    # Exibir a previsão
+    prediction_text = f'Previsão de Valor de Venda: kz{y_pred_novos_dados[0]:.2f}'
+    return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
